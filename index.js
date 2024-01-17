adfdab2799725ef77c0ab5f5514277d9037c2259
NazriHonest <naazir21@gmail.com>
2024-01-17 17:33:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import mongoose from "mongoose";
+import Tasks from "./models/tasksmodal.js";
+
+const app = express()
+
+app.use(express.json())
+
+const port = 8000;
+
+app.get('/', async (req, res) => {
+    const tasksList = await Tasks.find();
+    res.json(tasksList);
+})
+
+app.post('/', async(req, res) => {
+    const {title, date, finished} = req.body;
+
+    const newTask = new Tasks({
+        title, date, finished
+    });
+    const task = await newTask.save();
+    res.json(task);   
+})
+
+app.put('/:id', async(req, res) => {
+    const {title, date, finished} = req.body;
+
+    const task = await Tasks.findById(req.params.id);
+
+    if(task){
+        task.title = title,
+        task.date = date,
+        task.finished = finished
+
+        const updatedTask = await task.save();
+        res.json(updatedTask);
+    }   
+})
+
+app.delete('/:id', async(req, res) => {
+    const task = await Tasks.findByIdAndDelete(req.params.id);
+    res.json({message: "Task Deleted!"});   
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
+
+mongoose.connect("mongodb+srv://naazir21:naazir21@cluster0.c6aczf3.mongodb.net/mytasks?retryWrites=true&w=majority").then(()=>{
+    console.log("Connected to database");
+})
---
+++ models/tasksModal.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const tasksShema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+    },
+    date: {
+        type: String,
+        required: [true, "Date is required"],
+    },
+    finished: {
+        type: Boolean,
+        default: false, 
+    }
+});
+
+const Tasks = mongoose.model('tasks', tasksShema);
+
+export default Tasks;
